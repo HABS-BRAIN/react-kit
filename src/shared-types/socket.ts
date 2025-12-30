85ee64f53afb74e580b4a6cd374e071262c87022
Bogdan Gorelkin <b.gorelkin@yandex.ru>
2025-12-30 16:37:26 +0300

    studySelected event

--- src/shared-types/socket.ts
+++ src/shared-types/socket.ts
@@ -57,6 +57,7 @@ export interface OperatorClientEvents {
   }: SelectedParticipantPayload) => void;
   getConnections: () => void;
   protocolSelected: (protocol: Protocol) => void;
+  studySelected: (studyId: string, participantId: string) => void;
   playbackControl: (newPlaybackState: PlaybackState) => void;
   startFakeDeviceStream: () => void;
   stopFakeDeviceStream: () => void;
