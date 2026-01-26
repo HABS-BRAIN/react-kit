7780037b23821124201dd3b301f4421b1d811225
Bogdan Gorelkin <b.gorelkin@yandex.ru>
2026-01-26 10:17:33 +0100

    BLOCK_STEP

--- src/shared-types/step-meta.ts
+++ src/shared-types/step-meta.ts
@@ -1,4 +1,4 @@
-import { MAIN_EMOTION } from "./emotions"
+import { MAIN_EMOTION } from './emotions'
 
 export type StepMeta = { lslMarkerKey?: MarkerKeyWithStart } & Record<string, any>
 
@@ -9,12 +9,13 @@ export const LSLMarkerKey = {
   BASELINE_EYES_CLOSED: 'BASELINE_EYES_CLOSED',
   BLINKS: 'BLINKS',
   MOVE_EYES_LR: 'MOVE_EYES_LR',
-  MOVE_EYES_UD: "MOVE_EYES_UD",
+  MOVE_EYES_UD: 'MOVE_EYES_UD',
   MOVE_HEAD_LR: 'MOVE_HEAD_LR',
   MOVE_HEAD_UD: 'MOVE_HEAD_UD',
   CLENCH_JAW: 'CLENCH_JAW',
-  SWALLOW: "SWALLOW",
+  SWALLOW: 'SWALLOW',
   STUDY_BLOCK: 'STUDY_BLOCK',
+  BLOCK_STEP: 'BLOCK_STEP',
   PLAYBACK: 'PLAYBACK',
 } as const
 
@@ -25,107 +26,112 @@ export type MarkerKeyWithStart = Exclude<LSLMarkerKey, 'PLAYBACK'>
 type StartFinishMarker = { STARTED: number; FINISHED: number }
 type PlaybackMarker = { RESUMED: number; PAUSED: number }
 
-export type MarkerKeyIntV1Type =
-  Record<MarkerKeyWithStart, StartFinishMarker> & { PLAYBACK: PlaybackMarker }
+export type MarkerKeyIntV1Type = Record<MarkerKeyWithStart, StartFinishMarker> & {
+  PLAYBACK: PlaybackMarker
+}
 
 export const MarkerKeyIntV1: MarkerKeyIntV1Type = {
-	STUDY_BLOCK: {
-		STARTED: 10020,
-		FINISHED: 10021,
-	},
-	BASELINE_EYES_OPEN: {
-		STARTED: 10000,
-		FINISHED: 10001,
-	},
-	BASELINE_EYES_OPEN_NO_BLINKS: {
-		STARTED: 10002,
-		FINISHED: 10003,
-	},
-	BASELINE_EYES_CLOSED: {
-		STARTED: 10004,
-		FINISHED: 10005,
-	},
-	BLINKS: {
-		STARTED: 10006,
-		FINISHED: 10007,
-	},
-	MOVE_EYES_LR: {
-		STARTED: 10008,
-		FINISHED: 10009,
-	},
-	MOVE_EYES_UD: {
-		STARTED: 10010,
-		FINISHED: 100011,
-	},	
-	MOVE_HEAD_LR: {
-		STARTED: 10012,
-		FINISHED: 10013,
-	},
-	MOVE_HEAD_UD: {
-		STARTED: 10014,
-		FINISHED: 100015,
-	},		
-	CLENCH_JAW: {
-		STARTED: 10016,
-		FINISHED: 10017,
-	},
-	SWALLOW: {
-		STARTED: 10018,
-		FINISHED: 100019,
-	},		
-  PLAYBACK: {
-    RESUMED: 10022,
-    PAUSED: 100023,
+  BASELINE_EYES_OPEN: {
+    STARTED: 10000,
+    FINISHED: 10001,
   },
-	// Emotion markers (XXYYZ):
-	// Negative/Neutral category = 10, Positive/Curiosity category = 11
-	// Z: even = STARTED (0), odd = FINISHED (1)
-	ANGER: {
-		STARTED: 10100,
-		FINISHED: 10101,
-	},
-	FEAR: {
-		STARTED: 10200,
-		FINISHED: 10201,
-	},
-	DISGUST: {
-		STARTED: 10300,
-		FINISHED: 10301,
-	},
-	SADNESS: {
-		STARTED: 10400,
-		FINISHED: 10401,
-	},
-	JOY: {
-		STARTED: 11050,
-		FINISHED: 11051,
-	},
-	SURPRISE: {
-		STARTED: 11060,
-		FINISHED: 11061,
-	},
-	EXCITEMENT: {
-		STARTED: 11070,
-		FINISHED: 11071,
-	},
-	RELAXATION: {
-		STARTED: 11080,
-		FINISHED: 11081,
-	},
-	GUILT: {
-		STARTED: 10900,
-		FINISHED: 10901,
-	},
-	SENSUALITY: {
-		STARTED: 11100,
-		FINISHED: 11101,
-	},
-	CURIOSITY: {
-		STARTED: 11110,
-		FINISHED: 11111,
-	},
-	NEUTRAL: {
-		STARTED: 11200,
-		FINISHED: 11201,
-	},
+  BASELINE_EYES_OPEN_NO_BLINKS: {
+    STARTED: 10002,
+    FINISHED: 10003,
+  },
+  BASELINE_EYES_CLOSED: {
+    STARTED: 10004,
+    FINISHED: 10005,
+  },
+  BLINKS: {
+    STARTED: 10006,
+    FINISHED: 10007,
+  },
+  MOVE_EYES_LR: {
+    STARTED: 10008,
+    FINISHED: 10009,
+  },
+  MOVE_EYES_UD: {
+    STARTED: 10010,
+    FINISHED: 100011,
+  },
+  MOVE_HEAD_LR: {
+    STARTED: 10012,
+    FINISHED: 10013,
+  },
+  MOVE_HEAD_UD: {
+    STARTED: 10014,
+    FINISHED: 100015,
+  },
+  CLENCH_JAW: {
+    STARTED: 10016,
+    FINISHED: 10017,
+  },
+  SWALLOW: {
+    STARTED: 10018,
+    FINISHED: 100019,
+  },
+  STUDY_BLOCK: {
+    STARTED: 10020,
+    FINISHED: 10021,
+  },
+  PLAYBACK: {
+	RESUMED: 10022,
+	PAUSED: 100023,
+  },
+  BLOCK_STEP: {
+    STARTED: 10024,
+    FINISHED: 10025,
+  },
+  // Emotion markers (XXYYZ):
+  // Negative/Neutral category = 10, Positive/Curiosity category = 11
+  // Z: even = STARTED (0), odd = FINISHED (1)
+  ANGER: {
+    STARTED: 10100,
+    FINISHED: 10101,
+  },
+  FEAR: {
+    STARTED: 10200,
+    FINISHED: 10201,
+  },
+  DISGUST: {
+    STARTED: 10300,
+    FINISHED: 10301,
+  },
+  SADNESS: {
+    STARTED: 10400,
+    FINISHED: 10401,
+  },
+  JOY: {
+    STARTED: 11050,
+    FINISHED: 11051,
+  },
+  SURPRISE: {
+    STARTED: 11060,
+    FINISHED: 11061,
+  },
+  EXCITEMENT: {
+    STARTED: 11070,
+    FINISHED: 11071,
+  },
+  RELAXATION: {
+    STARTED: 11080,
+    FINISHED: 11081,
+  },
+  GUILT: {
+    STARTED: 10900,
+    FINISHED: 10901,
+  },
+  SENSUALITY: {
+    STARTED: 11100,
+    FINISHED: 11101,
+  },
+  CURIOSITY: {
+    STARTED: 11110,
+    FINISHED: 11111,
+  },
+  NEUTRAL: {
+    STARTED: 11200,
+    FINISHED: 11201,
+  },
 }
