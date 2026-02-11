ba9bb4898e65aa338a21408f282daa750d87b8c5
Bogdan Gorelkin <b.gorelkin@yandex.ru>
2026-02-11 17:37:26 +0100

    _id string|null

--- src/shared-types/study.ts
+++ src/shared-types/study.ts
@@ -15,10 +15,10 @@ export enum StudiesOrderBy {
 }
 
 export interface SequenceBlockCommonProps {
-  _id?: string
-  templateId?: string
+  _id: string | null
   title: string
   description?: string
+  templateId?: string
 }
 
 export interface StudyFullInfo extends Omit<Study, 'sequence'> {
