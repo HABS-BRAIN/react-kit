ae16a757d1ef00b0991dc8884e15403f5afb7326
Bogdan Gorelkin <b.gorelkin@yandex.ru>
2026-02-17 13:09:35 +0100

    {       step: Step       type: protocol     }   | {       step: Field       type: form     }

--- src/extractCurrentStep.ts
+++ src/extractCurrentStep.ts
@@ -5,18 +5,36 @@ import { CurrentPosition, StudyFullInfo } from './shared-types/study'
 export function extractCurrentStep(
   studyFullInfo: StudyFullInfo,
   currentPosition: CurrentPosition,
-): { step: Step | Field; type: 'protocol' | 'form' } {
+):
+  | {
+      step: Step
+      type: 'protocol'
+    }
+  | {
+      step: Field
+      type: 'form'
+    } {
   const { blockIndex, stepIndex } = currentPosition
   const block = studyFullInfo.sequence[blockIndex]
   if (!block) {
     throw new Error(`Block with index ${blockIndex} not found in study sequence`)
   }
-  const steps = block.type === 'protocol' ? block.protocol.steps : block.form.fields
-  const step = steps[stepIndex]
+  
+  if (block.type === 'protocol') {
+    const step = block.protocol.steps[stepIndex]
+    if (!step) {
+      throw new Error(
+        `Step with index ${stepIndex} not found in block at index ${blockIndex}`,
+      )
+    }
+    return { step, type: 'protocol' }
+  }
+
+  const step = block.form.fields[stepIndex]
   if (!step) {
     throw new Error(
       `Step with index ${stepIndex} not found in block at index ${blockIndex}`,
     )
   }
-  return { step, type: block.type }
+  return { step, type: 'form' }
 }
