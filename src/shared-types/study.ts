bccc9ebe97f94893b85f577957a0d3fab2b2904f
Bogdan Gorelkin <b.gorelkin@yandex.ru>
2026-03-23 09:36:07 +0100

    SequenceBlockItem for transfer Component

--- src/shared-types/study.ts
+++ src/shared-types/study.ts
@@ -28,6 +28,8 @@ export interface StudyFullInfo extends MongoDocumentFields, Omit<Study, 'sequenc
         type: 'protocol'
         runNextBlockForEachStep?: number // How many next blocks should run for each step
         nextBlockExecutes?: 'before' | 'after'
+        beforeBlocks?: SequenceBlockItem[]
+        afterBlocks?: SequenceBlockItem[]
         mixSteps?: boolean
         templateId: string
         events?: any[]
@@ -36,6 +38,8 @@ export interface StudyFullInfo extends MongoDocumentFields, Omit<Study, 'sequenc
         type: 'form'
         runNextBlockForEachStep?: number // How many next blocks should run for each step
         nextBlockExecutes?: 'before' | 'after'
+        beforeBlocks?: SequenceBlockItem[]
+        afterBlocks?: SequenceBlockItem[]
         mixSteps?: boolean
         templateId: string
         events?: any[]
@@ -50,24 +54,22 @@ export interface CompletedStudy extends StudyFullInfo {
   participantId: string //keycloak user id of the participant
 }
 
+export interface SequenceBlockItem {
+  type: 'protocol' | 'form'
+  _id: Protocol['_id'] | Form['_id']
+  mixSteps?: boolean
+}
+
 export interface Study {
   _id: string
   title: string
   children?: Study['_id'][]
   description?: string
-  sequence: {
-    type: 'protocol' | 'form'
-    _id: Protocol['_id'] | Form['_id']
+  sequence: (SequenceBlockItem & {
     runNextBlockForEachStep?: number
     nextBlockExecutes?: 'before' | 'after'
-    mixSteps?: boolean
-  }[]
+    beforeBlocks?: SequenceBlockItem[]
+    afterBlocks?: SequenceBlockItem[]
+  })[]
   belongsToOrganization: Organization['id'][]
 }
-
-// export interface StudyFullInfo extends Omit<Study, 'belongsToOrganization' | 'protocols' | 'preForms' | 'afterForms'> {
-//   belongsToOrganization: Organization[]
-//   protocols: Protocol[]
-//   preForms?: Form[]
-//   afterForms?: Form[]
-// }
