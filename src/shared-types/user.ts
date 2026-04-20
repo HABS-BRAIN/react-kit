36b3a3a8218766c6d67b80309555a49b525b322b
Bogdan Gorelkin <b.gorelkin@yandex.ru>
2026-04-20 13:55:36 +0200

    UserWithParticipantInfo

--- src/shared-types/user.ts
+++ src/shared-types/user.ts
@@ -1,3 +1,5 @@
+import { Calibration } from './study'
+
 export interface KeycloakUser {
   email: string
   emailVerified: boolean
@@ -11,4 +13,21 @@ export interface KeycloakUser {
 //for operator guides
 export enum GUIDE_PAGE {
   SETTING_UP_PARTICIPANT = 'SETTING_UP_PARTICIPANT',
+}
+
+export type UserWithParticipantInfo = KeycloakUser & {
+  participantInfo?: {
+    createdStudies?: string[]
+    calibration?: Calibration
+    finishedStudies?: Array<{
+      studyEventsFilename: string
+      studyEventsUrl?: string
+      studyId: string
+    }>
+  } | null
+}
+
+export type UsersListResponse = {
+  items: UserWithParticipantInfo[]
+  total: number
 }
